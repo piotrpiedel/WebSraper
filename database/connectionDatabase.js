26b9a54a0763d2e2039e3fc9fa09ad32d38adf32
piotrpiedel <piotrpiedel96@gmail.com>
2019-11-30 22:43:37 +0100

    Simply added tables and example of insert;

--- database/connectionDatabase.js
+++ database/connectionDatabase.js
@@ -2,16 +2,26 @@ var alasql = require('alasql');
 
 var mybase = new alasql.Database();
 
-const table_product = "CREATE TABLE product (id INT UNIQUE PRIMARY KEY NOT NULL ,name STRING)";
-const table_product_shop = "CREATE TABLE product_shop (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_shop = "CREATE TABLE shop (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_review = "CREATE TABLE review (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_review_comment = "CREATE TABLE review_comment (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_user = "CREATE TABLE user (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_questions = "CREATE TABLE questions (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_questions_answers = "CREATE TABLE questions_answers (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-const table_product_information = "CREATE TABLE product_information (id INT PRIMARY KEY , shop_price_product STRING, product_id_fk INT, shops_id_fk INT )";
-
-function createTables() {
-    mybase.exec(table_product)
-}
+const table_product = "CREATE TABLE product (id INT UNIQUE PRIMARY KEY NOT NULL, name varchar(40))";
+const table_product_shop = "CREATE TABLE product_shop (id INT PRIMARY KEY, shop_price_product varchar(20), product_id_fk INT, shops_id_fk INT )";
+const table_shop = "CREATE TABLE shop (id INT PRIMARY KEY, shop_price_product varchar(20))";
+const table_review = "CREATE TABLE review (id INT PRIMARY KEY, review_stars varchar(20), review_opinion INT, upvotes INT, downvotes INT, date_creation DATE, product_id_fk INT,created_by_user_id INT) ";
+const table_review_comment = "CREATE TABLE review_comment (id INT PRIMARY KEY, review_stars varchar(20), date_creation DATE, review_id INT, created_by_user_id INT)";
+const table_user = "CREATE TABLE user (id INT PRIMARY KEY, user_name varchar(20))";
+const table_questions = "CREATE TABLE questions (id INT PRIMARY KEY , question_string STRING, date_creation DATE, upvotes INT, downvotes INT, created_by_user_id INT )";
+const table_questions_answers = "CREATE TABLE questions_answers (id INT PRIMARY KEY , date_creation date, upvotes INT, downvotes INT, created_by_user_id INT, question_id INT)";
+const table_product_information = "CREATE TABLE product_information (id INT PRIMARY KEY , product_description varchar(700), product_producent varchar(100), product_id_fk int)";
+alasql(table_product,table_product_shop);
+alasql("INSERT INTO product VALUES (1, 'Product1')");
+alasql(['SELECT * FROM product'])
+    .then(function(res){
+        console.log(res); // output depends on
+    }).catch(function(err){
+    console.log('Does the file exist? There was an error:', err);
+});
+// mybase.exec(table_product,table_product_shop,table_shop,table_review,table_review_comment,table_user,table_questions,table_questions_answers,table_product_information);
+console.log("Reach that far");
+// function createTables() {
+//     mybase.exec(table_product,table_product_shop,table_shop,table_review,table_review_comment,table_user,table_questions,table_questions_answers,table_product_information);
+//     console.log("Reach that far")
+// }
