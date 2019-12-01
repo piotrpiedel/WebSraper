fdc596096411a36f5892073dcd09866736567080
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-01 01:22:33 +0100

    changed from alasql to db.exec for future use db.compile sql statement - kind of prepared statement from java;

--- database/connectionDatabase.js
+++ database/connectionDatabase.js
@@ -1,6 +1,5 @@
 var alasql = require('alasql');
-
-var mybase = new alasql.Database();
+var database = new alasql.Database();
 
 const table_product = "CREATE TABLE product (id INT UNIQUE PRIMARY KEY NOT NULL, name varchar(40))";
 const table_product_shop = "CREATE TABLE product_shop (id INT PRIMARY KEY, shop_price_product varchar(20), product_id_fk INT, shops_id_fk INT )";
@@ -11,17 +10,40 @@ const table_user = "CREATE TABLE user (id INT PRIMARY KEY, user_name varchar(20)
 const table_questions = "CREATE TABLE questions (id INT PRIMARY KEY , question_string STRING, date_creation DATE, upvotes INT, downvotes INT, created_by_user_id INT )";
 const table_questions_answers = "CREATE TABLE questions_answers (id INT PRIMARY KEY , date_creation date, upvotes INT, downvotes INT, created_by_user_id INT, question_id INT)";
 const table_product_information = "CREATE TABLE product_information (id INT PRIMARY KEY , product_description varchar(700), product_producent varchar(100), product_id_fk int)";
-(() => {
-    alasql(table_product, table_product_shop);
-    alasql("INSERT INTO product VALUES (1, 'Product1')");
-    alasql(['SELECT * FROM product'])
-        .then(function (res) {
-            console.log(res); // output depends on
-        }).catch(function (err) {
-        console.log('Does the file exist? There was an error:', err);
-    });
-    console.log("Reach that far");
-    console.log("Reach that far");
+
+async function alterTableWithRelations() {
+
+}
+
+async function createTables() {
+    // database.exec(table_product);
+    database.exec(table_product + ";"
+        + table_product_shop + ";"
+        + table_shop + ";"
+        + table_review + ";"
+        + table_review_comment + ";"
+        + table_user + ";"
+        + table_questions + ";"
+        + table_questions_answers + ";"
+        + table_product_information + ";"
+    );
+}
+
+(async () => {
+    await createTables();
+    await alterTableWithRelations();
+
+    // database.exec("INSERT INTO product VALUES (1, 'Product1')");
+    // console.log("Database created");
+    // database.exec('SELECT * FROM product', [], function(res){
+    //     console.log(res);
+    // });
+
+    // database.exec("INSERT INTO product_shop VALUES (1, 'Product1',20,25)");
+    // console.log("Database created");
+    // database.exec('SELECT * FROM product_shop', [], function(res){
+    //     console.log(res);
+    // });
 })();
 
 
@@ -44,10 +66,3 @@ const table_product_information = "CREATE TABLE product_information (id INT PRIM
 // ALTER TABLE `question_answer` ADD FOREIGN KEY (`question_id`) REFERENCES `question` (`id`);
 //
 // ALTER TABLE `product_information` ADD FOREIGN KEY (`product_id_fk`) REFERENCES `product` (`id`);
-
-// mybase.exec(table_product,table_product_shop,table_shop,table_review,table_review_comment,table_user,table_questions,table_questions_answers,table_product_information);
-
-// function createTables() {
-//     mybase.exec(table_product,table_product_shop,table_shop,table_review,table_review_comment,table_user,table_questions,table_questions_answers,table_product_information);
-//     console.log("Reach that far")
-// }
