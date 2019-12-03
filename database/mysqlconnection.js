f14ab09c49210e4dbf46808ddd56288edc6f848e
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-03 23:58:06 +0100

    Route product initial

--- database/mysqlconnection.js
+++ database/mysqlconnection.js
@@ -2,38 +2,16 @@ const mysql = require('mysql2');
 
 // https://www.techiediaries.com/node-mysql-database-crud/
 
-const connection = mysql.createConnection({
+const databaseConnection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'QTDW3qnbeYPPkIJddIR9',
     database: 'web_scraper_schema'
 });
 
-connection.connect(function(err) {
+databaseConnection.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
 
-connection.query('INSERT INTO product SET ?', ["name 001","name001@email.com"], (err, res) => {
-    if(err) throw err;
-});
-
-connection.query('SELECT * FROM product', (err,rows) => {
-    if(err) throw err;
-    console.log(rows);
-});
-
-connection.query(
-    'UPDATE product SET email = ? Where ID = ?',
-    ['updated@email.com', 1],
-    (err, result) => {
-        if (err) throw err;
-    }
-);
-
-
-connection.query(
-    'DELETE FROM contacts where id = ?', [1], (err, result) => {
-        if (err) throw err;
-    }
-);
+module.exports = databaseConnection;
