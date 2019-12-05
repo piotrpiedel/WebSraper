557614dc06293ab03c76e0626642a6d26d66a868
Kamil Tomsia <kamiltomsia@gmail.com>
2019-12-05 12:22:09 +0100

    scrapper takes comments now, questions almost done

--- webscrapper/webscrapper.js
+++ webscrapper/webscrapper.js
@@ -2,48 +2,61 @@ const Apify = require("apify");
 const url = require("url");
 const fs = require("fs");
 
-const {log} = Apify.utils;
+const { log } = Apify.utils;
 log.setLevel(log.LEVELS.WARNING);
 
-const productId = 41546872;
+const productId = 10201710;
 (async () => {
-    const numberOfPages = await getNumberOfPages(productId);
-    const reviews = await getReviews(numberOfPages);
+    const numberOfReviewsAndQuestions = await getNumberOfReviewsAndQuestions(
+        productId
+    );
+    const numberOfReviewPages = Math.ceil(
+        numberOfReviewsAndQuestions.numberOfReviews / 10
+    );
+    // const numberOfQuestionPages = Math.ceil(
+    //     numberOfReviewsAndQuestions.numberOfQuestions / 10
+    // );
+    const reviews = await getReviews(numberOfReviewPages);
+    // const questions = await getQuestions(numberOfQuestionPages);
     saveDataToJsonFile(reviews);
 })();
 
-async function getNumberOfPages(productId) {
+async function getNumberOfReviewsAndQuestions(productId) {
     const requestList = new Apify.RequestList({
-        sources: [{url: `https://www.ceneo.pl/${productId}`}]
+        sources: [{ url: `https://www.ceneo.pl/${productId}` }]
     });
     await requestList.initialize();
 
     let numberOfReviews = 0;
+    let numberOfQuestions = 0;
     const crawler = new Apify.CheerioCrawler({
         requestList,
         minConcurrency: 10,
         maxConcurrency: 50,
         maxRequestRetries: 1,
         handlePageTimeoutSecs: 60,
-        handlePageFunction: async ({request, body, $}) => {
+        handlePageFunction: async ({ request, body, $ }) => {
             numberOfReviews = $(".reviews a")
                 .attr("title")
                 .replace(/\D/g, "");
+            numberOfQuestions = $(".questions a")
+                .attr("title")
+                .replace(/\D/g, "");
         },
-        handleFailedRequestFunction: async ({request}) => {
+        handleFailedRequestFunction: async ({ request }) => {
             console.log(`Request ${request.url} failed twice.`);
         }
     });
 
     await crawler.run();
 
-    return Math.ceil(numberOfReviews / 10);
+    return { numberOfReviews, numberOfQuestions };
 }
 
 async function getReviews(numberOfPages) {
     const urls = [];
     for (let i = 1; i <= numberOfPages; i++) {
-        urls.push({url: `https://www.ceneo.pl/${productId}/opinie-${i}`});
+        urls.push({ url: `https://www.ceneo.pl/${productId}/opinie-${i}` });
     }
     const requestList = new Apify.RequestList({
         sources: urls
@@ -58,7 +71,7 @@ async function getReviews(numberOfPages) {
         maxConcurrency: 50,
         maxRequestRetries: 1,
         handlePageTimeoutSecs: 60,
-        handlePageFunction: async ({request, body, $}) => {
+        handlePageFunction: async ({ request, body, $ }) => {
             const reviewData = $("li.review-box");
             reviewData.each((index, el) => {
                 const elObj = $(el);
@@ -101,6 +114,29 @@ async function getReviews(numberOfPages) {
                 });
                 const numberOfUpVotes = elObj.find(".vote-yes span").text();
                 const numberOfDownVotes = elObj.find(".vote-no span").text();
+
+                const comments = [];
+                elObj
+                    .find("li.product-review-comment")
+                    .each((index, element) => {
+                        const comment = {};
+                        const header = $(element)
+                            .first()
+                            .first()
+                            .text()
+                            .split("\n")
+                            .filter(element => element.trim().length > 0);
+
+                        comment.id = $(element).attr("data-entry-id");
+                        comment.author = header[0].trim();
+                        comment.message = header[2].trim();
+
+                        comment.dateOfComment = $(element)
+                            .find(".review-time")
+                            .children()[0].attribs.datetime;
+                        comments.push(comment);
+                    });
+
                 reviews.push({
                     id,
                     author,
@@ -113,11 +149,12 @@ async function getReviews(numberOfPages) {
                     advantages,
                     disadvantages,
                     numberOfUpVotes,
-                    numberOfDownVotes
+                    numberOfDownVotes,
+                    comments
                 });
             });
         },
-        handleFailedRequestFunction: async ({request}) => {
+        handleFailedRequestFunction: async ({ request }) => {
             console.log(`Request ${request.url} failed twice.`);
         }
     });
@@ -128,8 +165,12 @@ async function getReviews(numberOfPages) {
 
 function saveDataToJsonFile(data) {
     console.log(__dirname);
-    fs.writeFile(url.resolve(__dirname, 'data\\data.json'), JSON.stringify(data, null, 4), error => {
-        if (error) return;
-        console.log("Data has been succesfully saved!");
-    });
+    fs.writeFile(
+        url.resolve(__dirname, "data\\data.json"),
+        JSON.stringify(data, null, 4),
+        error => {
+            if (error) return;
+            console.log("Data has been succesfully saved!");
+        }
+    );
 }
