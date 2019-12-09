23a37ab498a0e9063067bd9fb8581cb0619e4d5f
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-09 21:42:26 +0100

    Read and parse file to json

--- jsonparser/reviewparser.js
+++ jsonparser/reviewparser.js
@@ -2,18 +2,14 @@
 const url = require("url");
 const fs = require("fs");
 
-(async () => {
 
-    fs.readFile(url.resolve(__dirname, "data\\data.json"), (error, data) => {
-        if (error) {
-            console.error(error);
-            throw error;
-        }
-        let parsedReviews = JSON.parse(data);
-        console.log(parsedReviews);
-    });
-})();
+function readDataFile() {
+    var file = fs.readFileSync(url.resolve(__dirname, "data\\data.json"));
+    console.log("file is read ok");
+    return JSON.parse(file);
+}
 
+exports.readDataFile = readDataFile();
 
 // var jsonData = JSON.parse();
 // for (var i = 0; i < jsonData.counters.length; i++) {
