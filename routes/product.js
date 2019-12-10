87bdff4e5cc53518a03e8e4d91850428180b7ea9
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-10 22:31:10 +0100

    routes get;

--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,9 @@ const express = require("express");
 const Router = express.Router();
 const databaseConnection = require("../database/mysqlconnection");
 const Product = require("../models/product");
-// http://localhost:3000/product/123
 Router.get("/:productID", (request, response) => {
     console.log(request.params.productID);
-    var newporductionstest= new Product(request.params.productID,'gfgff');
-    // console.log(newporductionstest.name);
-    response.send(Product.createOrUpdateProduct(newporductionstest));
+    response.send(Product.getProductById(request.params.productID));
 });
 
 module.exports = Router;
