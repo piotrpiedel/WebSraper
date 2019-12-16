0530bebc832780e6e0eb492e5d11bad10c826622
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-16 22:32:41 +0100

    Modified questionTransformed file logic

--- transformservice/transformQuestionData.js
+++ transformservice/transformQuestionData.js
@@ -1,25 +1,43 @@
 'use strict';
 const Question = require("../models/question");
+const QuestionAnswer = require("../models/questionAnswer");
 const FileUtil = require("../utils/fileUtil");
 
-async function transformQuestionData(questions) {
-    questions.forEach(question => {
-        question.author = question.author
-            .split(" ")[0]
-            .replace(/\r|\n|\t/g, "");
-        question.dateOfQuestion = new Date(question.dateOfQuestion).getTime();
-        question.title = question.title.trim();
-        question.message = question.message.trim();
-        question.numberOfUpVotes = Number(question.numberOfUpVotes);
-        question.numberOfDownVotes = Number(question.numberOfDownVotes);
+const productId = 10201710;
+(async () => {
+    await transformQuestionData(productId, FileUtil.readDataFile("dataextracted", "questions"));
+})();
 
+async function transformQuestionData(productId, questions) {
+    const arrayOfParsedQuestionModels = [];
+    const arrayOfParsedQuestionAnswerModels = [];
+    questions.forEach(question => {
+        let questionModel = new Question(question.id,
+            question.title.trim(),
+            question.message.trim(),
+            new Date(question.dateOfQuestion).getTime(),
+            question.author
+                .split(" ")[0]
+                .replace(/\r|\n|\t/g, ""),
+            Number(question.numberOfUpVotes),
+            Number(question.numberOfDownVotes),
+            productId
+        );
+        arrayOfParsedQuestionModels.push(questionModel);
         question.answers.forEach(answer => {
-            answer.author = answer.author.trim();
-            answer.message = answer.message.trim().replace(/\r|\n|\t/g, "");
-            answer.dateOfAnswer = new Date(answer.dateOfAnswer).getTime();
-            answer.numberOfUpVotes = Number(answer.numberOfUpVotes);
-            answer.numberOfDownVotes = Number(answer.numberOfDownVotes);
+            let questionAnswerModel = new QuestionAnswer(
+                answer.id,
+                answer.message.trim().replace(/\r|\n|\t/g, ""),
+                new Date(answer.dateOfAnswer).getTime(),
+                answer.author.trim(),
+                Number(answer.numberOfUpVotes),
+                Number(answer.numberOfDownVotes),
+                question.id
+            );
+            arrayOfParsedQuestionAnswerModels.push(questionAnswerModel);
         });
     });
+    FileUtil.saveDataToJsonFile("datatransfromed", "questionsTransformed", arrayOfParsedQuestionModels);
+    FileUtil.saveDataToJsonFile("datatransfromed", "questionAnswersTransformed", arrayOfParsedQuestionAnswerModels);
 }
 
