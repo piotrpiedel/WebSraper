e296c11f75f0974d3863d389fd3d718785588166
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-18 23:20:06 +0100

    save product or many products service;

--- services/productService.js
+++ services/productService.js
@@ -3,15 +3,26 @@ const Product = require("../models/product");
 const FileUtil = require("../utils/fileUtil");
 const {transformProductData} = require("../transformservice/transformProductData");
 
-async function saveProductToDatabase(productData) {
-    let productModel = new Product.Builder(productData.id)
-        .withName(productData.productName.trim())
-        .withProducer(productData.producer.trim())
-        .build();
-    FileUtil.saveDataToJsonFile("datatransfromed", "productTransformed", productModel);
-}
+(async () => {
+    await saveProductsToDatabase(FileUtil.readDataFile("datatransfromed", "productTransformed"));
+})();
 
-async function transformProductDataFromDataExtracted(productId) {
-    let productData = await FileUtil.readDataFile("dataextracted", "productData");
-    await transformProductData(productId, productData)
+async function saveProductsToDatabase(productData) {
+    if (Array.isArray(productData)) {
+        for (const product of productData) {
+            let productModel = new Product.Builder(productData.id)
+                .withName(productData.name.trim())
+                .withProducer(productData.producer.trim())
+                .build();
+            await Product.createOrUpdateProduct(productModel);
+        }
+        console.debug("saveProductToDatabase productData", productData);
+    } else {
+        let productModel = new Product.Builder(productData.id)
+            .withName(productData.name.trim())
+            .withProducer(productData.producer.trim())
+            .build();
+        await Product.createOrUpdateProduct(productModel);
+        console.debug("Product was saved with success", productModel);
+    }
 }
