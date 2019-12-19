f442cd4c24638a0027defec167a311a59b66876d
piotrpiedel <piotrpiedel96@gmail.com>
2019-12-19 23:29:40 +0100

    trying to get stats;

--- services/productService.js
+++ services/productService.js
@@ -1,28 +1,41 @@
 'use strict';
 const Product = require("../models/product");
 const FileUtil = require("../utils/fileUtil");
-// const {transformProductData} = require("../transformservice/transformProductData");
+const databaseEnum = require("../config/database_enum");
 
 (async () => {
     await saveProductsToDatabase(FileUtil.readDataFile("datatransfromed", "productTransformed"));
 })();
 
+async function saveProductToDatabase(product, productInsertedStats, productUpdatedStats) {
+    let productModel = new Product.Builder(product.id)
+        .withName(product.name.trim())
+        .withProducer(product.producer.trim())
+        .build();
+    let value = await Product.createOrUpdateProduct(productModel);
+    if (value === databaseEnum.INSERT) {
+        productInsertedStats++
+    } else if (value === databaseEnum.UPDATE) {
+        productUpdatedStats++
+    }
+    return {productInsertedStats, productUpdatedStats};
+}
+
 async function saveProductsToDatabase(productData) {
+    let productUpdatedStats = 0;
+    let productInsertedStats = 0;
     if (Array.isArray(productData)) {
         for (const product of productData) {
-            let productModel = new Product.Builder(product.id)
-                .withName(product.name.trim())
-                .withProducer(product.producer.trim())
-                .build();
-            await Product.createOrUpdateProduct(productModel);
+            const value = await saveProductToDatabase(product, productInsertedStats, productUpdatedStats);
+            productInsertedStats = value.productInsertedStats;
+            productUpdatedStats = value.productUpdatedStats;
         }
-        console.debug("Saved products to database successfully");
+        console.debug("productInsertedStats, productUpdatedStats: ", productInsertedStats, productUpdatedStats);
+        return {productInsertedStats, productUpdatedStats};
     } else {
-        let productModel = new Product.Builder(productData.id)
-            .withName(productData.name.trim())
-            .withProducer(productData.producer.trim())
-            .build();
-        await Product.createOrUpdateProduct(productModel);
-        console.debug("Product was saved with success", productModel);
+        const __ret = await saveProductToDatabase(productData, productInsertedStats, productUpdatedStats);
+        productInsertedStats = __ret.productInsertedStats;
+        productUpdatedStats = __ret.productUpdatedStats;
+        return {productInsertedStats, productUpdatedStats};
     }
 }
